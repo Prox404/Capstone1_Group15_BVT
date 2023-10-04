b41d3dd741e9c132efcd2c28facb4190f5875b7b
ngocanh0202 <89017204+ngocanh0202@users.noreply.github.com>
2023-10-05 02:29:44 +0700

    create_vaccination ver 2.0

--- app/src/main/java/com/prox/babyvaccinationtracker/MainActivity.java
+++ app/src/main/java/com/prox/babyvaccinationtracker/MainActivity.java
@@ -2,16 +2,28 @@ package com.prox.babyvaccinationtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, create_vaccination.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/prox/babyvaccinationtracker/create_vaccination.java
+++ app/src/main/java/com/prox/babyvaccinationtracker/create_vaccination.java
@@ -2,15 +2,18 @@ package com.prox.babyvaccinationtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.prox.babyvaccinationtracker.model.Vaccines;
+
 
 public class create_vaccination extends AppCompatActivity {
 
@@ -18,6 +21,14 @@ public class create_vaccination extends AppCompatActivity {
     Button btn_tt;
     ImageButton img;
 
+    DatePickerDialog datePickerDialog;
+    boolean is_input(String a){
+        if(a.length() == 0){
+            Toast.makeText(this, "Phải nhập thông tin", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +50,78 @@ public class create_vaccination extends AppCompatActivity {
         btn_tt = findViewById(R.id.btn_tt);
 
         img = findViewById(R.id.img);
+
+        Calendar currentDate = Calendar.getInstance();
+        // Khởi tạo DatePickerDialog
+        datePickerDialog = new DatePickerDialog(
+                create_vaccination.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Xử lý khi người dùng chọn ngày
+                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                        edt_date_of_entry.setText(selectedDate);
+                    }
+                },
+                currentDate.get(Calendar.YEAR), // Năm mặc định
+                currentDate.get(Calendar.MONTH), // Tháng mặc định
+                currentDate.get(Calendar.DAY_OF_MONTH) // Ngày mặc định
+        );
+        datePickerDialog.getDatePicker().setMaxDate(currentDate.getTimeInMillis());
+
+        // Ngăn việc nhập trực tiếp vào EditText
+        edt_date_of_entry.setFocusable(false);
+        edt_date_of_entry.setClickable(true);
+
+        edt_date_of_entry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
+            }
+        });
+
         btn_tt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String vaccine_name = edt_vaccine_name.getText().toString();
+                if(is_input(vaccine_name) == false){
+                    edt_vaccine_name.requestFocus();
+                    return;
+                }
                 String vac_effectiveness = edt_vac_effectiveness.getText().toString();
+                if(is_input(vac_effectiveness) == false){
+                    edt_vac_effectiveness.requestFocus();
+                    return;
+                }
                 String post_vaccination_reactions = edt_post_vaccination_reactions.getText().toString();
+                if(is_input(post_vaccination_reactions) == false){
+                    edt_post_vaccination_reactions.requestFocus();
+                    return;
+                }
                 String origin = edt_origin.getText().toString();
+                if(is_input(origin) == false){
+                    edt_origin.requestFocus();
+                    return;
+                }
                 int vaccination_target_group = 0;
                 try{
                     vaccination_target_group = Integer.parseInt(edt_vaccination_target_group.getText().toString());
                 }catch (Exception e){
-                    edt_vaccination_target_group.setText("ERROR");
                     edt_vaccination_target_group.requestFocus();
                     Toast.makeText(create_vaccination.this, "Phải nhập số", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 String contraindications = edt_contraindications.getText().toString();
+                if(is_input(contraindications)==false){
+                    edt_contraindications.requestFocus();
+                    return;
+                }
 
                 int quantity = 0;
                 try {
                     quantity= Integer.parseInt(edt_quantity.getText().toString());
                 }catch (Exception e){
-                    edt_quantity.setText("ERROR");
                     edt_quantity.requestFocus();
                     Toast.makeText(create_vaccination.this, "Phải nhập số", Toast.LENGTH_SHORT).show();
                     return;
@@ -72,22 +131,30 @@ public class create_vaccination extends AppCompatActivity {
                 try{
                     dosage = Integer.parseInt(edt_dosage.getText().toString());
                 }catch (Exception e){
-                    edt_dosage.setText("ERROR");
                     edt_dosage.requestFocus();
                     Toast.makeText(create_vaccination.this, "Phải nhập số", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 String unit = edt_unit.getText().toString();
+                if(is_input(unit) == false){
+                    edt_unit.requestFocus();
+                    return;
+                }
                 String date_of_entry = edt_date_of_entry.getText().toString();
+                if(date_of_entry.length() == 0){
+                    edt_date_of_entry.requestFocus();
+                    Toast.makeText(create_vaccination.this, "Phải chọn ngày nhập cảnh ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 double price = 0;
                 try{
                     price = Double.parseDouble(edt_price.getText().toString());
                 }catch (Exception e){
-                    edt_price.setText("ERROR");
                     edt_price.requestFocus();
                     Toast.makeText(create_vaccination.this, "Phải nhập số", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
 
