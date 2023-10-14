a5144f5b3fa75ccad521e39d6ab505e59f796f5f
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-10-14 22:32:22 +0700

    get Notification

--- app/src/main/java/com/prox/babyvaccinationtracker/MainActivity.java
+++ app/src/main/java/com/prox/babyvaccinationtracker/MainActivity.java
@@ -1,20 +1,26 @@
 package com.prox.babyvaccinationtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cloudinary.android.MediaManager;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.prox.babyvaccinationtracker.service.NotificationService;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Map config = new HashMap();
+    final int MY_FOREGROUND_SERVICE_PERMISSION_REQUEST_CODE = 1;
+    final int MY_BOOT_COMPLETED_PERMISSION_REQUEST_CODE = 2;
     private void configCloudinary() {
         config.put("cloud_name", "du42cexqi");
         config.put("api_key", "346965553513552");
@@ -27,6 +33,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
         configCloudinary();
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.FOREGROUND_SERVICE) == PackageManager.PERMISSION_GRANTED) {
+            Log.i("main", "FOREGROUND_SERVICE Permission granted");
+        } else {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.FOREGROUND_SERVICE}, MY_FOREGROUND_SERVICE_PERMISSION_REQUEST_CODE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECEIVE_BOOT_COMPLETED) == PackageManager.PERMISSION_GRANTED) {
+            Log.i("main", "RECEIVE_BOOT_COMPLETED Permission granted");
+        } else {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_BOOT_COMPLETED}, MY_FOREGROUND_SERVICE_PERMISSION_REQUEST_CODE);
+        }
+
+        Intent notificationService = new Intent(MainActivity.this, NotificationService.class);
+        startService(notificationService);
 
         Intent i = new Intent(MainActivity.this, AuthActivity.class);
         startActivity(i);
---
+++ app/src/main/java/com/prox/babyvaccinationtracker/service/NotificationService.java
@@ -0,0 +1,138 @@
+package com.prox.babyvaccinationtracker.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.prox.babyvaccinationtracker.NotificationActivity;
+import com.prox.babyvaccinationtracker.R;
+import com.prox.babyvaccinationtracker.model.NotificationMessage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class NotificationService extends Service {
+    public NotificationService() {
+    }
+
+    private DatabaseReference databaseReference;
+    private String user_id;
+    private String notificationChannelId;
+    private NotificationManager notificationManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
+        user_id = sharedPreferences.getString("customer_id", "");
+        Log.i("Notification_service", "Run Service");
+
+        // Khởi tạo Firebase Database Reference
+        databaseReference = FirebaseDatabase.getInstance().getReference("notifications").child(user_id);
+
+        // Khởi tạo NotificationManager
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Tạo Notification Channel (chỉ cần thực hiện trên Android 8.0+)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(user_id, "Appointment Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Lắng nghe sự thay đổi trong cơ sở dữ liệu
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                // Xử lý khi có lịch hẹn mới được thêm vào
+                NotificationMessage notificationMessage = dataSnapshot.getValue(NotificationMessage.class);
+                if (notificationMessage != null) {
+                    scheduleNotification(notificationMessage); // Lên lịch hiển thị thông báo
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                // Xử lý khi có sự thay đổi trong lịch hẹn (nếu cần)
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                // Xử lý khi có lịch hẹn bị xóa (nếu cần)
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                // Xử lý khi có sự thay đổi vị trí của lịch hẹn (nếu cần)
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Xử lý khi có lỗi xảy ra trong cơ sở dữ liệu (nếu cần)
+            }
+        });
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void scheduleNotification(NotificationMessage notification) {
+        Log.i("Notification_service", "schedule notification");
+        // Định dạng thời gian lấy từ cơ sở dữ liệu
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+        Date notificationTime = notification.getDate();
+
+        // Kiểm tra nếu thời gian lịch hẹn đã qua thì không hiển thị thông báo
+        if (notificationTime != null && notificationTime.after(new Date())) {
+            // Lên lịch hiển thị thông báo
+            Handler handler = new Handler();
+            handler.postDelayed(() -> showNotification(notification), notificationTime.getTime() - System.currentTimeMillis());
+        }
+    }
+
+    // Phương thức để hiển thị thông báo
+    private void showNotification(NotificationMessage message) {
+        Intent intent = new Intent(this, NotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new NotificationCompat.Builder(this, notificationChannelId)
+                .setContentTitle(message.getTitle())
+                .setContentText(message.getMessage())
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build();
+
+//        notificationManager.notify(0, notification);
+        startForeground((int)(Math.random() * 10000) + 1, notification);
+    }
+
+}
