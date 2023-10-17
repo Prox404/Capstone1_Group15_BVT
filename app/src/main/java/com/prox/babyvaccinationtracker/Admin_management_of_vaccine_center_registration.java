a029ef2c619e800aa348e147006b5080ae933b2d
ngocanh0202 <89017204+ngocanh0202@users.noreply.github.com>
2023-10-17 23:11:42 +0700

    Admin confirms

---
+++ app/src/main/java/com/prox/babyvaccinationtracker/Admin_management_of_vaccine_center_registration.java
@@ -0,0 +1,60 @@
+package com.prox.babyvaccinationtracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.prox.babyvaccinationtracker.adapter.CenterRegistrationAdapter;
+import com.prox.babyvaccinationtracker.model.Vaccine_center_registration;
+
+import java.util.ArrayList;
+
+public class Admin_management_of_vaccine_center_registration extends AppCompatActivity {
+
+    RecyclerView confirm_vaccine_center_registration;
+
+    ArrayList<Vaccine_center_registration> registrations = new ArrayList<>();
+    CenterRegistrationAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_management_of_vaccine_center_registration);
+
+        confirm_vaccine_center_registration = findViewById(R.id.confirm_vaccine_center_registration);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference vaccineRef = database.getReference("Vaccine_center_registration");
+
+        vaccineRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snap : snapshot.getChildren()){
+                    int status = snap.child("status").getValue(int.class);
+                    if(status==0){
+                        Vaccine_center_registration regi = snap.getValue(Vaccine_center_registration.class);
+                        regi.setCenter_registration_id(snap.getKey());
+                        registrations.add(regi);
+                    }
+                    adapter = new CenterRegistrationAdapter(Admin_management_of_vaccine_center_registration.this,registrations);
+                    confirm_vaccine_center_registration.setLayoutManager(new GridLayoutManager(Admin_management_of_vaccine_center_registration.this,1));
+                    confirm_vaccine_center_registration.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.i("Firebasseeeee", ""+error);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/prox/babyvaccinationtracker/adapter/CenterRegistrationAdapter.java
@@ -0,0 +1,85 @@
+package com.prox.babyvaccinationtracker.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.prox.babyvaccinationtracker.R;
+import com.prox.babyvaccinationtracker.VaccineCenterRegistration;
+import com.prox.babyvaccinationtracker.model.Vaccine_center_registration;
+
+import java.util.ArrayList;
+
+public class CenterRegistrationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    ArrayList<Vaccine_center_registration> Vaccine_center_registration;
+    Context context;
+    VaccineCenterRegistration vaccineCenterRegistration;
+    public CenterRegistrationAdapter(Context context,ArrayList<Vaccine_center_registration> vaccineCenterRegistration){
+        this.context = context;
+        this.Vaccine_center_registration = vaccineCenterRegistration;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_vaccine_center_registration, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Vaccine_center_registration re = Vaccine_center_registration.get(position);
+        Log.i("HOLLDER", ""+holder);
+        if (holder instanceof ViewHolder) {
+            ViewHolder viewHolder = (ViewHolder) holder;
+            viewHolder.tv_name.setText(re.getCenter().getCenter_name());
+            viewHolder.tv_email.setText(re.getCenter().getCenter_email());
+            viewHolder.btn_confirm_vaccine.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(context == null){
+                        Log.i("Contentttttttt",""+context);
+                        return;
+                    }
+                    if(re.getCenter() == null){
+                        Log.i("Centerttttttt",""+re.getCenter());
+                        return;
+                    }
+                    vaccineCenterRegistration = new VaccineCenterRegistration();
+                    vaccineCenterRegistration.registerCenter(context,re.getCenter(),re.getCenter_registration_id());
+
+                    Vaccine_center_registration.remove(position);
+                    notifyDataSetChanged();
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if(Vaccine_center_registration != null)
+            return Vaccine_center_registration.size();
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_name, tv_email;
+        Button btn_confirm_vaccine;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tv_name = itemView.findViewById(R.id.tv_name);
+            tv_email = itemView.findViewById(R.id.tv_email);
+            btn_confirm_vaccine = itemView.findViewById(R.id.btn_confirm_vaccine);
+        }
+    }
+}
