20f19e378c7d2ac3d16f4a8ee98fbd575d472a3a
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-10-25 15:43:35 +0700

    add activity

---
+++ app/src/main/java/com/prox/babyvaccinationtracker/AddHealthInformationActivity.java
@@ -0,0 +1,14 @@
+package com.prox.babyvaccinationtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddHealthInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_health_information);
+    }
+}
