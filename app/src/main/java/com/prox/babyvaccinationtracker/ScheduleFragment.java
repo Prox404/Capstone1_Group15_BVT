83d3025b7c19e1d74ec9e1236cc2bc209409620f
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-11-27 02:12:11 +0700

    -- thêm lịch  tiêm chủng ở tab lịch trình

--- app/src/main/java/com/prox/babyvaccinationtracker/ScheduleFragment.java
+++ app/src/main/java/com/prox/babyvaccinationtracker/ScheduleFragment.java
@@ -1,18 +1,40 @@
 package com.prox.babyvaccinationtracker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.prox.babyvaccinationtracker.adapter.PendingRequestAdapter;
+import com.prox.babyvaccinationtracker.adapter.RegistrationTimelineAdapter;
+import com.prox.babyvaccinationtracker.model.Vaccination_Registration;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScheduleFragment extends Fragment {
 
     LinearLayout pendingInjection, scheduleInjection;
+    RecyclerView recyclerViewTimeline;
+    Context context;
+    List<Vaccination_Registration> vaccination_registrations = new ArrayList<>();
 
 
     public ScheduleFragment() {
@@ -37,8 +59,40 @@ public class ScheduleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        context = container != null ? container.getContext() : null;
         scheduleInjection = view.findViewById(R.id.scheduleInjection);
         pendingInjection = view.findViewById(R.id.pendingInjection);
+        recyclerViewTimeline = view.findViewById(R.id.recyclerViewTimeline);
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("user", Context.MODE_PRIVATE);
+        String id_vaccine_center = sharedPreferences.getString("customer_id","");
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Vaccination_Registration");
+        Query query = databaseReference.orderByChild("cus/customer_id").equalTo(id_vaccine_center);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                vaccination_registrations.clear();
+                Log.i("Pending", "onDataChange: " + snapshot.getChildrenCount());
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Vaccination_Registration vaccination_registration = dataSnapshot.getValue(Vaccination_Registration.class);
+                    if(vaccination_registration.getStatus() == 0){
+                        vaccination_registration.setRegist_id(dataSnapshot.getKey());
+                        vaccination_registrations.add(vaccination_registration);
+                    }
+                }
+                Log.i("Pending", "onDataChange: " + vaccination_registrations.size());
+                RegistrationTimelineAdapter pendingRequestAdapter = new RegistrationTimelineAdapter( getContext() , vaccination_registrations);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                recyclerViewTimeline.setAdapter(pendingRequestAdapter);
+                recyclerViewTimeline.setLayoutManager(linearLayoutManager);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         scheduleInjection.setOnClickListener(new View.OnClickListener() {
             @Override
